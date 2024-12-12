ee1eaade9faa58c4b52d682ae10e1be47f230ab9
NatarajanKeyboard <127331286+NatarajanKeyboard@users.noreply.github.com>
2024-12-12 16:38:35 +0530

    Update LiftModel.java

--- Zoho_Test_12-12-24/Lift/src/LiftModel.java
+++ Zoho_Test_12-12-24/Lift/src/LiftModel.java
@@ -1,9 +1,44 @@
 public class LiftModel {
 
     private LiftView liftView;
+    private String assignedLift;
 
     public LiftModel(LiftView liftView)
     {
         this.liftView=liftView;
     }
+
+    public String getAssignedLift()
+    {
+        return assignedLift;
+    }
+
+    // public void assignLift(int[] liftArray, int source, int destination)
+    // {
+    //     assignedLift="L1";
+    //     liftArray[0]=destination;
+        
+    // }
+
+    public void assignNearestLift(int[] liftArray, int source, int destination)
+    {
+        int min=10_000;
+        int minIndex=0;
+        for(int index=4;index>=0;index--)
+        {
+            int floor=liftArray[index];
+            System.out.println(Math.abs(floor-source));
+            if(Math.abs(floor-source)<min)
+            {
+                min=floor;
+                minIndex=index;
+            }
+        }
+        System.out.println(min+" "+minIndex);
+        assignedLift="L"+(minIndex+1);
+        System.out.println(assignedLift);
+        liftArray[minIndex]=destination;
+        
+    }
+
 }
