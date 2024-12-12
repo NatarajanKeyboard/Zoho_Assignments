f19808bd6d696b7a3a9eb82a2978514ef81484b8
NatarajanKeyboard <127331286+NatarajanKeyboard@users.noreply.github.com>
2024-12-12 16:37:47 +0530

    Update App.java

--- Zoho_Test_12-12-24/Lift/src/App.java
+++ Zoho_Test_12-12-24/Lift/src/App.java
@@ -6,12 +6,13 @@ public class App {
     }
     public static void main(String[] input)
     {
-        int[] liftArray={0,0,0,0,0};        //the initial lift positions
+        int[] liftArray={5,2,7,9,0};        //the initial lift positions
+
 
         // converting the input 
         int source=Integer.parseInt(input[0]);
         int destination=Integer.parseInt(input[1]);
 
-        new LiftView().displayLiftPosition(liftArray,source,destination);
+        new LiftView().init(liftArray,source,destination);
     }
 }
