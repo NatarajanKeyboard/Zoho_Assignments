f682a68a3d76aa64ceee8ea87d6e77e28f0b3c5f
NatarajanKeyboard <127331286+NatarajanKeyboard@users.noreply.github.com>
2024-12-12 16:39:04 +0530

    Update LiftView.java

--- Zoho_Test_12-12-24/Lift/src/LiftView.java
+++ Zoho_Test_12-12-24/Lift/src/LiftView.java
@@ -7,11 +7,39 @@ public class LiftView {
         liftModel=new LiftModel(this);
     }
 
-    
-
-    public void displayLiftPosition(int[] liftArray, int source, int destination)
+    public void init(int[] liftArray, int source, int destination)
     {
-        System.out.println("Lift  : L1 L2 L3 L4 L5");
-        System.out.println("Floor : "+liftArray[0]+"  "+liftArray[1]+"  "+liftArray[2]+"  "+liftArray[3]+"  "+liftArray[4]);
+        displayLiftPosition(liftArray, source, destination,null);
+        // liftModel.assignLift(liftArray, source, destination);
+        //after assigning
+        // displayLiftPosition(liftArray, source, destination,liftModel.getAssignedLift());
+
+        //after assigning nearest lift
+        liftModel.assignNearestLift(liftArray, source, destination);
+        displayLiftPosition(liftArray, source, destination,liftModel.getAssignedLift());
+
     }
+
+
+    // public void displayLiftPosition(int[] liftArray, int source, int destination)
+    // {
+    //     System.out.println("Lift  : L1 L2 L3 L4 L5");
+    //     System.out.println("Floor : "+liftArray[0]+"  "+liftArray[1]+"  "+liftArray[2]+"  "+liftArray[3]+"  "+liftArray[4]+"\n\n\n");
+    // }
+    //overloading displayLiftPosition()
+
+
+    public void displayLiftPosition(int[] liftArray, int source, int destination,String assignedLift)
+    {
+        if(assignedLift!=null)      
+        {
+            System.out.println(assignedLift+" is assigned");
+        }
+
+        System.out.println("Lift  : L1 L2 L3 L4 L5");
+        System.out.println("Floor : "+liftArray[0]+"  "+liftArray[1]+"  "+liftArray[2]+"  "+liftArray[3]+"  "+liftArray[4]+"\n\n\n");
+    }
+
+
+
 }
